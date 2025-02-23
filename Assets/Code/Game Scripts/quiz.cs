a80880710cf39e8c7271cc36c2156bcefc211318
hbeaumont2204 <harrybsmith06@yahoo.co.uk>
2025-02-23 00:34:44 +0000

    Update

--- Assets/Code/Game Scripts/quiz.cs
+++ Assets/Code/Game Scripts/quiz.cs
@@ -1,38 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System.Threading.Tasks;
+using System.IO;
+using System;
 
 public class quiz  : MonoBehaviour
 {
     public question[] questions;
     public int currentQuestionNumber = 0;
+    question currentQuestion;
     public int score = 0;
-    int timer = 30;
+    float timer = 30;
     
     public Text questionDisplay;
     public Text scoreDisplay;
     public Text resultDisplay;
-
+    
+    // Called at the start
     private void Start()
     {
         setup();
     }
 
+    // Called every frame
     void Update()
     {
-        timer = timer - time.DeltaTime;
+        timer = timer - Time.deltaTime;
     }
 
     void setup()
     {
+        string[] allQuestions;
+        string[][] allChoices;
+        string[] allAnswers;
+        
+    }
 
+    string[] ReadFile(string path)
+    {
+        string[] data = { };
+        if (File.Exists(path))
+        {
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Array.Resize(ref data, data.Length + 1);
+                    data[data.Length - 1] = line;
+                    Debug.Log(line);
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("Error");
+        }
+        return data;
+    }
+
+    string[][] GetChoices(string path)
+    {
+        string[][] data = { };
+        if (File.Exists(path))
+        {
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] options = line.Split(',');
+                    Array.Resize(ref data, data.Length + 1);
+                    data[data.Length - 1] = options;
+                    Debug.Log(line);
+                }
+                
+            }
+        }
+        else
+        {
+            Debug.Log("Error");
+        }
+        return data;
     }
 
     void displayQuestion()
     {
         if (currentQuestionNumber < questions.Length)
         {
-            question currentQuestion = questions[currentQuestionNumber];
+            currentQuestion = questions[currentQuestionNumber];
             questionDisplay.text = currentQuestion.questionText;
             resultDisplay.text = "";
         }
@@ -42,7 +100,7 @@ public class quiz  : MonoBehaviour
         }
     }
 
-    void checkAnswer(int choice)
+    void checkAnswer(int choice, question currentQuestion)
     {
         if (choice == currentQuestion.correctChoice)
         {
@@ -62,25 +120,22 @@ public class quiz  : MonoBehaviour
     {
         score = score + 10;
         scoreDisplay.text = score.ToString();
-        await Task.Delay(5000);
         currentQuestionNumber++;
-        displayQuestion();
+        Invoke("displayQuestion", 5.0f);
     }
 
     void incorrectAnswer()
     {
         score = score - 5;
         scoreDisplay.text = score.ToString();
-        await Task.Delay(5000);
         currentQuestionNumber++;
-        displayQuestion();
+        Invoke("displayQuestion", 5.0f);
     }
 
     void skipQuestion()
     {
-        await Task.Delay(5000);
         currentQuestionNumber++;
-        displayQuestion();
+        Invoke("displayQuestion", 5.0f);
     }
 
     void endQuiz()
@@ -88,28 +143,29 @@ public class quiz  : MonoBehaviour
 
     }
 
+    // Choice Buttons
     void choiceA()
     {
-        checkAnswer(1);
+        checkAnswer(1, currentQuestion);
     }
     
     void choiceB()
     {
-        checkAnswer(2);
+        checkAnswer(2, currentQuestion);
     }
 
     void choiceC()
     {
-        checkAnswer(3);
+        checkAnswer(3, currentQuestion);
     }
 
     void choiceD()
     {
-        checkAnswer(4);
+        checkAnswer(4, currentQuestion);
     }
 
     void skip()
     {
-        checkAnswer(-1);
+        checkAnswer(-1, currentQuestion);
     }
 }
