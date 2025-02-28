2cffff95ae86c8198d9abef90926de205ee664cb
hbeaumont2204 <harrybsmith06@yahoo.co.uk>
2025-02-28 17:26:20 +0000

    Update to UI and Code

--- Assets/Code/Game Scripts/question.cs
+++ Assets/Code/Game Scripts/question.cs
@@ -1,8 +1,16 @@
 using UnityEngine;
 
-public class question : MonoBehaviour
+public class question
 {
     public string questionText; // The question
     public string[] choices; // A list of 4 choices
     public int correctChoice; // The index of the correct choice (1-4)
+
+    // Constructor
+    public question(string questionText, string[] choices, int correctChoice)
+    {
+        this.questionText = questionText;
+        this.choices = choices;
+        this.correctChoice = correctChoice;
+    }
 }
--- Assets/Code/Game Scripts/quiz.cs
+++ Assets/Code/Game Scripts/quiz.cs
@@ -5,18 +5,26 @@ using UnityEngine.UI;
 using System.Threading.Tasks;
 using System.IO;
 using System;
+using TMPro;
 
 public class quiz  : MonoBehaviour
 {
-    public question[] questions;
-    public int currentQuestionNumber = 0;
-    question currentQuestion;
-    public int score = 0;
-    float timer = 30;
-    
-    public Text questionDisplay;
-    public Text scoreDisplay;
-    public Text resultDisplay;
+    public question[] questions = { }; // Array containing questions
+    public int currentQuestionNumber = 0; // Index of the current question
+    question currentQuestion; // The current question
+    public int score = 10; // Player Score
+    public bool questionActive;
+    float timer = 30; // 30 seconds to answer every question
+    // UI Display
+    public TextMeshProUGUI questionDisplay;
+    public TextMeshProUGUI scoreDisplay;
+    public TextMeshProUGUI resultDisplay;
+    public TextMeshProUGUI choiceA;
+    public TextMeshProUGUI choiceB;
+    public TextMeshProUGUI choiceC;
+    public TextMeshProUGUI choiceD;
+    public TextMeshProUGUI timerDisplay;
+    [SerializeField] private Slider timerSlider;
     
     // Called at the start
     private void Start()
@@ -27,17 +35,48 @@ public class quiz  : MonoBehaviour
     // Called every frame
     void Update()
     {
-        timer = timer - Time.deltaTime;
+        if (timer > 0 && questionActive)
+        {
+            timer = timer - Time.deltaTime; // Timer 
+            timerDisplay.text = (Convert.ToInt16(timer)).ToString();
+            timerSlider.value = timer / 30;
+            if (timer <= 15 && timer > 5)
+            {
+                timerDisplay.color = Color.yellow;
+            }
+            if (timer <= 5)
+            {
+                timerDisplay.color = Color.red;
+            }
+            
+        }
+        else if (timer <= 0 && questionActive)
+        {
+            Skip();
+        }
+
     }
 
+    // Takes questions, choices and the correct answer index from text files
     void setup()
     {
-        string[] allQuestions;
-        string[][] allChoices;
-        string[] allAnswers;
-        
+        string[] allQuestions = ReadFile("Assets/Files/Questions.txt");
+        string[][] allChoices = GetChoices("Assets/Files/Choices.txt");
+        string[] allAnswers =  ReadFile("Assets/Files/Answers.txt");
+        // Adds every question to the array questions.
+        for (int i = 0; i < allAnswers.Length; i++)
+        {
+            int answer = Convert.ToInt32(allAnswers[i]);
+            question newQuestion = new question(allQuestions[i], allChoices[i], answer);
+            Array.Resize(ref questions, questions.Length + 1);
+            questions[questions.Length - 1] = newQuestion;
+            //Debug.Log(allQuestions[i]);
+            //Debug.Log(answer);
+        }
+        displayQuestion();
     }
 
+    // Reads data from a text file
     string[] ReadFile(string path)
     {
         string[] data = { };
@@ -50,7 +89,7 @@ public class quiz  : MonoBehaviour
                 {
                     Array.Resize(ref data, data.Length + 1);
                     data[data.Length - 1] = line;
-                    Debug.Log(line);
+                    //Debug.Log(line);
                 }
             }
         }
@@ -60,7 +99,7 @@ public class quiz  : MonoBehaviour
         }
         return data;
     }
-
+    // Used to read the choices from a text file
     string[][] GetChoices(string path)
     {
         string[][] data = { };
@@ -74,9 +113,9 @@ public class quiz  : MonoBehaviour
                     string[] options = line.Split(',');
                     Array.Resize(ref data, data.Length + 1);
                     data[data.Length - 1] = options;
-                    Debug.Log(line);
+                    //Debug.Log(line);
                 }
-                
+
             }
         }
         else
@@ -85,14 +124,21 @@ public class quiz  : MonoBehaviour
         }
         return data;
     }
-
+    // Displays the current question
     void displayQuestion()
     {
         if (currentQuestionNumber < questions.Length)
         {
+            timer = 30;
+            timerDisplay.color = Color.green;
+            questionActive = true;
             currentQuestion = questions[currentQuestionNumber];
             questionDisplay.text = currentQuestion.questionText;
             resultDisplay.text = "";
+            choiceA.text = currentQuestion.choices[0];
+            choiceB.text = currentQuestion.choices[1];
+            choiceC.text = currentQuestion.choices[2];
+            choiceD.text = currentQuestion.choices[3];
         }
         else
         {
@@ -118,54 +164,94 @@ public class quiz  : MonoBehaviour
 
     void correctAnswer()
     {
-        score = score + 10;
+        score = score + 10; // 10 points added for every correct answer
         scoreDisplay.text = score.ToString();
+        if (score > 0)
+        {
+            scoreDisplay.color = Color.green;
+        }
+        resultDisplay.text = "Correct answer";
+        resultDisplay.color = Color.green;
         currentQuestionNumber++;
-        Invoke("displayQuestion", 5.0f);
+        Invoke("displayQuestion", 5.0f); // Waits for 5 seconds before the next question
     }
 
     void incorrectAnswer()
     {
-        score = score - 5;
+        score = score - 5; // 5 points lost for a correct answer
         scoreDisplay.text = score.ToString();
+        if (score == 0)
+        {
+            scoreDisplay.color = Color.yellow;
+        }
+        else if (score < 0)
+        {
+            scoreDisplay.color = Color.red;
+        }
+        resultDisplay.text = "Incorrect answer";
+        resultDisplay.color = Color.red;
         currentQuestionNumber++;
-        Invoke("displayQuestion", 5.0f);
+        Invoke("displayQuestion", 5.0f); // 5 second delay
     }
 
     void skipQuestion()
     {
+        // No score change
+        resultDisplay.text = "No answer given";
+        resultDisplay.color = Color.yellow;
         currentQuestionNumber++;
-        Invoke("displayQuestion", 5.0f);
+        Invoke("displayQuestion", 5.0f); // 5 second delay
     }
-
+    // Displays end score
     void endQuiz()
     {
 
     }
 
     // Choice Buttons
-    void choiceA()
+    public void ChoiceA()
     {
-        checkAnswer(1, currentQuestion);
-    }
-    
-    void choiceB()
-    {
-        checkAnswer(2, currentQuestion);
-    }
-
-    void choiceC()
-    {
-        checkAnswer(3, currentQuestion);
+        if (questionActive)
+        {
+            questionActive = false;
+            checkAnswer(1, currentQuestion);
+        }
     }
 
-    void choiceD()
+    public void ChoiceB()
     {
-        checkAnswer(4, currentQuestion);
+        if (questionActive)
+        {
+            questionActive = false;
+            checkAnswer(2, currentQuestion);
+        }
+        
     }
 
-    void skip()
+    public void ChoiceC()
     {
-        checkAnswer(-1, currentQuestion);
+        if (questionActive)
+        {
+            questionActive = false;
+            checkAnswer(3, currentQuestion);
+        }  
+    }
+
+    public void ChoiceD()
+    {
+        if (questionActive)
+        {
+            questionActive = false;
+            checkAnswer(4, currentQuestion);
+        }
+    }
+
+    public void Skip()
+    {
+        if (questionActive)
+        {
+            questionActive = false;
+            checkAnswer(-1, currentQuestion);
+        }
     }
 }
