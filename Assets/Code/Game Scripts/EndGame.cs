30d005ea1c1f3aa69e67bf99a2a378dbfafcc896
hbeaumont2204 <harrybsmith06@yahoo.co.uk>
2025-03-01 23:12:06 +0000

    ..

--- Assets/Code/Game Scripts/EndGame.cs
+++ Assets/Code/Game Scripts/EndGame.cs
@@ -1,20 +1,38 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using System.Threading.Tasks;
+using System.IO;
+using System;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class EndGame : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public TextMeshProUGUI previousScore;
+    public TextMeshProUGUI highScore;
 
     // Update is called once per frame
     void Update()
     {
         
     }
-    public void quit_game()
+
+    public void mainMenu()
+    {
+        SceneManager.LoadSceneAsync("Main Menu");
+    }
+
+    public void quitGame()
     {
         Application.Quit();
+    }
+
+    string getScore(string path)
+    {
+        StreamReader sr = new StreamReader(path);
+        string line = sr.ReadLine();
+        return line;
     }
 }
--- Assets/Code/Main Menu Scripts/Scoreboard.cs
+++ Assets/Code/Main Menu Scripts/Scoreboard.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using System.Threading.Tasks;
+using System.IO;
+using System;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Scoreboard : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public TextMeshProUGUI previousScore;
+    public TextMeshProUGUI highScore;
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    string getScore(string path)
+    {
+        StreamReader sr = new StreamReader(path);
+        string line = sr.ReadLine();
+        return line;
     }
 }
